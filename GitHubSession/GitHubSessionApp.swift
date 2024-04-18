dc6e18f6576f57c0df86c245f8948261bbcbbb78
Lucid <bluecho97@gmail.com>
2024-04-18 20:13:23 +0900

    Initial Commit

---
+++ GitHubSession/GitHubSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSessionApp.swift
+//  GitHubSession
+//
+//  Created by bluecho97 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
